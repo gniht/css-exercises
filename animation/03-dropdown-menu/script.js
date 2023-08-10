02f91141cd3d5db7d04b7cc7c1f568fbe82c2868
gniht <gniht@pop-os.localdomain>
2023-08-10 14:51:36 -0700

    completed exercises

--- animation/03-dropdown-menu/script.js
+++ animation/03-dropdown-menu/script.js
@@ -1,10 +1,13 @@
 const dropdownContainer = document.querySelector(".dropdown-container");
 const menuTitle = document.querySelector(".menu-title");
 const dropdownMenu = document.querySelector(".dropdown-menu");
+const hello = document.querySelector(".hello");
+
 
 menuTitle.addEventListener("click", (e) => {
   if (e.target === e.currentTarget) {
-    dropdownMenu.classList.toggle("visible");
+    dropdownMenu.classList.toggle("visible");    
+       
   }  
 })
 
